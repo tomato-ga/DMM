df8cca7cd6e72a021b2ffb90d07d66c7a1a81930
tomato <servicedake@gmail.com>
2022-04-19 11:10:18 +0900

    gitignore追加。あとは4アカで回すreply完成

--- twitter_aff_videos/selenium_upload/0418_master_twit_upload_OtxSf.py
+++ twitter_aff_videos/selenium_upload/0418_master_twit_upload_OtxSf.py
@@ -94,7 +94,7 @@ class Tweet:
 
             # テキスト入力
             self.wait.until(EC.presence_of_all_elements_located)
-            text = '今日はこれでぬきぬき！→' + ' '+ f'{upload_url}'
+            text = '今日はこれでぬきぬき！'
             elem_text = self.driver.find_element(by=By.CLASS_NAME, value='notranslate')
             elem_text.click()
             elem_text.send_keys(text)
