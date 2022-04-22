f38121dc585d57941a89cfe993dff98447848b1b
tomato <servicedake@gmail.com>
2022-04-22 16:03:11 +0900

    ジャンルURLにとべないのはなぜか？

--- DMMAPI/test_spank_dmmapi.py
+++ DMMAPI/test_spank_dmmapi.py
@@ -1,8 +1,9 @@
+
 import os
-from dmm_api import DMMApiClient
+# from dmm_api import DMMApiClient
 import pprint
 import json
-
+from os import path
 
 APIID = os.environ.get('DMM_API_ID', 'b7fkZaG3pW6ZZHpGBbLz')
 AFFILIATEID = os.environ.get('DMM_AFFILIATE_ID', 'kamipanmen-990')
@@ -47,15 +48,30 @@ pprint.pprint(floor_item)
 
 #TODO ジャンルはJSONで保存しておくのがよさそう
 
-all_genre = requests.get(f'https://api.dmm.com/affiliate/v3/GenreSearch?api_id={APIID}&affiliate_id={AFFILIATEID}&floor_id=43&hits=500&offset=1&output=json')
-all_genre_text = all_genre.text
-all_genre_data = json.loads(all_genre_text)
-all_genre_item = all_genre_data['result']
+# initial=%E3%81%99& スパンキング
 
-for items in all_genre_item['genre']:
-    for item in items.values():
-        if 'スパンキング' in item:
-            spank_url = items['list_url']
+"""オールジャンル取得"""
+# all_genre = requests.get(f'https://api.dmm.com/affiliate/v3/GenreSearch?api_id={APIID}&affiliate_id={AFFILIATEID}&floor_id=43&hits=500&offset=1&output=json')
+# all_genre_text = all_genre.text
+# all_genre_data = json.loads(all_genre_text)
+# all_genre_item = all_genre_data['result']
 
-spank = requests.get(spank_url)
-print(spank)
+# with open('genre.json', 'w', encoding='utf-8') as f:
+#     json.dump(all_genre_item, f, indent=4, ensure_ascii=False)
+
+def jsonload():
+    with open('genre.json', 'r', encoding='utf-8') as r:
+        genre = json.load(r)
+        for items in genre['genre']:
+            if 'スパンキング' in items['name']:
+                page_url = items['list_url']
+                print(page_url)
+    return page_url
+
+def get_url(page_url):
+    spank = requests.get(page_url)
+    print(spank)
+
+
+page_url = jsonload()
+get_url(page_url)
