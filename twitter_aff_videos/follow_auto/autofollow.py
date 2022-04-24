040bbdb421c8925218e994445b5fd52546577634
tomato <servicedake@gmail.com>
2022-04-24 23:20:01 +0900

    自動フォロー修正中

--- twitter_aff_videos/follow_auto/autofollow.py
+++ twitter_aff_videos/follow_auto/autofollow.py
@@ -3,10 +3,13 @@ import follow_main
 import follow_parse
 import random
 
-import api_togsi7 as API
+import api_HjQhq as API
 
 pack = Get_follower()
 client = pack.apicall(API=API)
+
+
+###################アカウントで1000人フォローしてたときのスクリプト##########################
 
 """アカウントのフォロワー（フォローするID）を再取得する→
 差分取得→フォロー実施""" #TODO フォローするのを自動化する
@@ -18,21 +21,27 @@ client = pack.apicall(API=API)
 """フォロー実施"""
 # pack.follow10(client, follow_suru_ids)
 
+###################アカウントで1000人フォローしてたときのスクリプト##########################
+
+
+###################リストから最新ツイート取得してLIKEしてるユーザーをフォロー##########################
 
 response = client.get_list_tweets(id=1514978714572173313, max_results=15,  expansions=["attachments.media_keys","author_id","referenced_tweets.id"])
 tweets = response.data
 random.shuffle(tweets)
 
 like_user_follow = 0
-for tweet in tweets:
-    tid = tweet.id
-    like_users = client.get_liking_users(tid)
-    follows = like_users.data
+while like_user_follow < 5:
+    for tweet in tweets:
+        tid = tweet.id
+        like_users = client.get_liking_users(tid)
+        follows = like_users.data
 
-    if follows:
-        for follow in follows:
-            client.follow_user(target_user_id=follow.id)
-            like_user_follow += 1
-            if like_user_follow > 5:
-                break
+        if follows:
+            for follow in follows:
+                client.follow_user(target_user_id=follow.id)
+                like_user_follow += 1
+    if like_user_follow == 5:
+        break
 
+###################リストから最新ツイート取得してLIKEしてるユーザーをフォロー##########################
