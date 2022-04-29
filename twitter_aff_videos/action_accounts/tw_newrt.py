d3b02d713ff724acffeb04e94d0e116cc1b326fb
tomato <servicedake@gmail.com>
2022-04-29 15:34:33 +0900

    stop

--- twitter_aff_videos/action_accounts/tw_newrt.py
+++ twitter_aff_videos/action_accounts/tw_newrt.py
@@ -3,7 +3,7 @@ import api_tomorrow_genkio
 import api_1j_mc
 import api_togsi7
 
-
+#メインアカは停止
 #tw_package.My_new_rt(api_tomorrow_genkio, api_tomorrow_genkio.ids)
 
 tw_package.My_new_rt(api_1j_mc, api_1j_mc.ids)
