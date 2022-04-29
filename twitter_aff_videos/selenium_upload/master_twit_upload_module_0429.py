0a3f43b4deff323e03f5b6d66a1a63e3773d2292
tomato <servicedake@gmail.com>
2022-04-29 20:39:36 +0900

    コミットrestore

--- twitter_aff_videos/selenium_upload/master_twit_upload_module_0429.py
+++ twitter_aff_videos/selenium_upload/master_twit_upload_module_0429.py
@@ -15,6 +15,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome import service as fs
 
+# wait_1 = random.random()
+# wait_2 = random.randint(59,300)
+# wait = round(wait_1 + wait_2, 5)
+
 
 class Tweet:
 
@@ -22,17 +26,16 @@ class Tweet:
 
     def __init__(self):
         self.options = Options()
-        #self.options.add_argument('--headless')
+        self.options.add_argument('--headless')
         self.options.add_argument('--no-sandbox')
         self.options.add_argument('--ignore-certificate-errors')
         self.options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36')
-        self.driver = webdriver.Chrome('C:\\Users\\PC_User\\Documents\\GitHub\\kutikomi\\bakusai\\chromedriver.exe', options=self.options)
+        self.driver = webdriver.Chrome(options=self.options)
         # self.driver = webdriver.Chrome(options=self.options)  #options=self.options 'C:\\Users\\PC_User\\Documents\\GitHub\\kutikomi\\bakusai\\chromedriver.exe'
         #self.driver.implicitly_wait(10)
 
-        wait_1 = random.random()
-        wait_2 = random.randint(30,40)
-        self.randomwait = round(wait_1 + wait_2, 5)
+        self.wait1 = random.random()
+        self.wait2 = random.randint(3,6)
         self.wait = WebDriverWait(driver=self.driver, timeout=30)
         self.twitter = 'https://twitter.com/login'
 
@@ -93,7 +96,7 @@ class Tweet:
                 # ファイルパスを入力
                 """Ubuntuの場合、glob.globではなく、os.path.abspathにしたらアップできた!!"""
                 self.wait.until(EC.presence_of_all_elements_located)
-                video_path = os.path.abspath(f'X:\\don\\files\\twitvideo\\{upload_video_file_name}')     # Windows (f'X:\\don\\files\\twitvideo\\{upload_video_file_name}') #Ubuntu (f'/mnt/hdd/don/files/twitvideo/{upload_video_file_name}')
+                video_path = os.path.abspath(f'/mnt/hdd/don/files/twitvideo/{upload_video_file_name}')     # Windows (f'X:\\don\\files\\twitvideo\\{upload_video_file_name}') #Ubuntu (f'/mnt/hdd/don/files/twitvideo/{upload_video_file_name}')
                 self.driver.find_element(by=By.XPATH, value="//input[@type='file']").send_keys(video_path)
                 time.sleep(2)
 
@@ -106,10 +109,8 @@ class Tweet:
                 time.sleep(1)
 
                 # 投稿
-                # time.sleep(self.randomwait)
                 tweet_button = self.driver.find_element(by=By.XPATH, value='//*[@data-testid="tweetButtonInline"]')
                 tweet_button.click()
-
                 self.wait.until(EC.presence_of_all_elements_located)
 
         except Exception as ex:
