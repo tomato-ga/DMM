1fb0fc4f8a4797f8a0fbbc28627e10c640850b05
tomato <servicedake@gmail.com>
2022-05-02 01:01:53 +0900

    ubuntu dir

--- twitter_pic_download/pic_download.py
+++ twitter_pic_download/pic_download.py
@@ -52,6 +52,7 @@ class Image:
             blocks = soup.find('div', {'class': 'entry-content'})
             img_url = blocks.find_all('img')
             os.mkdir(f'/mnt/hdd/don/fliles/twitphotos/{title}')
+            os.chmod(f'/mnt/hdd/don/fliles/twitphotos/{title}', 755)
 
             for img in img_url:
                 self.img_urls.append(img['src'])
