75c1aab18d9954ea33169329f60e41fa70925ed4
tomato <52771764+tomato-ga@users.noreply.github.com>
2022-05-05 19:39:57 +0900

    headless

--- twitter_pic_upload/master_twit_photo_upload.py
+++ twitter_pic_upload/master_twit_photo_upload.py
@@ -25,7 +25,7 @@ class Tweet:
 
     def __init__(self):
         self.options = Options()
-        #self.options.add_argument('--headless')
+        self.options.add_argument('--headless')
         self.options.add_argument('--no-sandbox')
         self.options.add_argument('--ignore-certificate-errors')
         self.options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36')
