6df82c4cbc8917bb782c83b1ae1a9dba232ba029
tomato <52771764+tomato-ga@users.noreply.github.com>
2022-05-05 13:59:24 +0900

    もｐっかい

--- img_get_test.py
+++ img_get_test.py
@@ -7,11 +7,14 @@ json_path = open('./overf_all_photos.json')
 load = json.load(json_path)
 l = load['photo']
 
-for i in l:
-    print(i['img'])
-    im = requests.get(i['img'])
-    name_search = re.findall('([a-zA-z0-9_-]*)(.[a-z]{3,4}$)', im.url)
-    os.makedirs(r'/mnt/hdd/don/files/mizugazo/overf/', mode=0o777, exist_ok=True)
+try:
+    for i in l:
+        print(i['img'])
+        im = requests.get(i['img'])
+        name_search = re.findall('([a-zA-z0-9_-]*)(.[a-z]{3,4}$)', im.url)
+        os.makedirs('/mnt/hdd/don/files/mizugazo/overf/', mode=0o777, exist_ok=True)
 
-    with open(fr'/mnt/hdd/don/files/mizugazo/overf/{name_search[0][0]}{name_search[0][1]}', 'wb') as image:
-        image.write(im.content)
+        with open(f'/mnt/hdd/don/files/mizugazo/overf/{name_search[0][0]}{name_search[0][1]}', 'wb') as image:
+            image.write(im.content)
+except Exception as ex:
+    print(ex)
