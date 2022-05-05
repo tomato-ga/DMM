3c9dee5643d7207e93871f55f562998b20428d36
tomato <52771764+tomato-ga@users.noreply.github.com>
2022-05-05 14:05:35 +0900

    match case test

--- img_get_test.py
+++ img_get_test.py
@@ -12,11 +12,16 @@ try:
         print(i['img'])
         img_url = i['img']
         im_get = requests.get(img_url)
-        name_search = re.findall('([a-zA-z0-9_-]*)(.[a-z]{3,4}$)', im_get)
+        name_search = re.findall('([a-zA-z0-9_-]*)(.[a-z]{3,4}$)', im_get.url)
         os.makedirs('/mnt/hdd/don/files/mizugazo/overf/', mode=0o777, exist_ok=True)
 
-        with open(f'/mnt/hdd/don/files/mizugazo/overf/{name_search[0][0]}{name_search[0][1]}', 'wb') as image:
-            image.write(im_get.content)
-            print('保存完了')
+        match im_get.status_code:
+            case 200:
+                with open(f'/mnt/hdd/don/files/mizugazo/overf/{name_search[0][0]}{name_search[0][1]}', 'wb') as image:
+                    image.write(im_get.content)
+                    print('保存完了')
+            case _:
+                pass
+
 except Exception as ex:
     print(ex)
