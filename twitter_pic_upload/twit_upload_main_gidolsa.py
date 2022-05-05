0007fd994645ef295715b39bc70f6719e130b1cc
tomato <52771764+tomato-ga@users.noreply.github.com>
2022-05-05 16:31:12 +0900

    ハッシュタグ追加

--- twitter_pic_upload/twit_upload_main_gidolsa.py
+++ twitter_pic_upload/twit_upload_main_gidolsa.py
@@ -5,7 +5,7 @@ from master_twit_photo_upload import Tweet
 if __name__ == '__main__':
 
     i = Tweet()
-    i.Uploads(account='gidolsa', text='#グラビア'+ '\n' + '#グラビアアイドル')
+    i.Uploads(account='gidolsa', text='#グラビア'+ '\n' + '#グラビアアイドル' + '\n' + '#グラビア画像')
     i.Quit()
 
 
