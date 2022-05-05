4b5360ca443f85fb1f877aba5e28474ffe0fb8da
tomato <52771764+tomato-ga@users.noreply.github.com>
2022-05-05 14:01:01 +0900

    ぜんぜんちがったｗ

--- img_get_test.py
+++ img_get_test.py
@@ -10,11 +10,12 @@ l = load['photo']
 try:
     for i in l:
         print(i['img'])
-        im = requests.get(i['img'])
+        img_url = i['img']
+        im_get = requests.get(img_url)
         name_search = re.findall('([a-zA-z0-9_-]*)(.[a-z]{3,4}$)', im.url)
         os.makedirs('/mnt/hdd/don/files/mizugazo/overf/', mode=0o777, exist_ok=True)
 
         with open(f'/mnt/hdd/don/files/mizugazo/overf/{name_search[0][0]}{name_search[0][1]}', 'wb') as image:
-            image.write(im.content)
+            image.write(im_get.content)
 except Exception as ex:
     print(ex)
