ae98c492f56ff2b7277ca6d2771851ff0ba27c61
tomato <52771764+tomato-ga@users.noreply.github.com>
2022-05-05 13:56:37 +0900

    fストリング拡張し追加

--- img_get_test.py
+++ img_get_test.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+import re
 
 json_path = open('./overf_all_photos.json')
 load = json.load(json_path)
@@ -9,7 +10,8 @@ l = load['photo']
 for i in l:
     print(i['img'])
     im = requests.get(i['img'])
+    name_search = re.findall('([a-zA-z0-9_-]*)(.[a-z]{3,4}$)', im.url)
     os.makedirs(r'/mnt/hdd/don/files/mizugazo/overf/', mode=0o777, exist_ok=True)
 
-    with open(r'/mnt/hdd/don/files/mizugazo/overf/', 'wb') as image:
+    with open(fr'/mnt/hdd/don/files/mizugazo/overf/{name_search[0][0]}{name_search[0][1]}', 'wb') as image:
         image.write(im.content)
