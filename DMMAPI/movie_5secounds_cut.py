cc5bcef057af06dbc8a58941f4ebedcbe3a54a99
tomato <52771764+tomato-ga@users.noreply.github.com>
2022-05-08 19:47:06 +0900

    Boxでjson読み込み楽

--- DMMAPI/movie_5secounds_cut.py
+++ DMMAPI/movie_5secounds_cut.py
@@ -1,6 +1,6 @@
 from moviepy.editor import *
 
-def cut5():
+def cut5secounds():
 
     video_path = 'C:\\Users\\PC_User\\Documents\\GitHub\\DMM\\23test.mp4' #ビデオパスを入れる
     save_file_name = 'cuttest.mp4' #保存ファイル名
@@ -8,3 +8,14 @@ def cut5():
 
     videos = VideoFileClip(video_path).subclip(start)
     videos.write_videofile(save_file_name, fps=29)
+
+
+def cut2min():
+
+    video_path = '/Users/ore/Downloads/[FRIDAY] Risa Yukihira 雪平莉左 - Do you like a beautiful older sister 綺麗なお姉さんは、好きですか？ (2021-11-18)/yukihira.ts' #ビデオパスを入れる
+    save_file_name = 'cuttest.mp4' #保存ファイル名
+    start = 5
+
+    videos = VideoFileClip(video_path).subclip(start)
+    videos.write_videofile(save_file_name, fps=29)
+
