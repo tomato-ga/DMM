dec28b78d63d44b4150d0672490c1caa163937a3
tomato <52771764+tomato-ga@users.noreply.github.com>
2022-05-14 23:39:01 +0900

    いったんwpapi 更新分だけ取得

--- twitter_aff_videos/selenium_upload/twit_upload_main_tomorrow.py
+++ twitter_aff_videos/selenium_upload/twit_upload_main_tomorrow.py
@@ -2,9 +2,6 @@ from master_twit_upload_module_0510 import Tweet
 
 if __name__ == '__main__':
 
-    try:
-        i = Tweet()
-        i.Uploads(account='tomorrow_genkio', text='この動画を特定したぞ→')
-        i.Quit()
-    except:
-        logger.exception(sys.exc_info())
+    i = Tweet()
+    i.Uploads(account='tomorrow_genkio', text='この動画を特定したぞ→')
+    i.Quit()
