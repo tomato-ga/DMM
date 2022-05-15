74dc4158f2b81f03e7973c6b0c138caff4c37e02
tomato-ga <servicedake@gmail.com>
2022-05-15 22:52:28 +0900

    カット成功

--- DMMAPI/movie_5secounds_cut.py
+++ DMMAPI/movie_5secounds_cut.py
@@ -2,18 +2,22 @@ from moviepy.editor import *
 import os
 import random
 
+
 def cut5secounds(directory):
 
     vs = os.listdir(directory)
-    random.shuffle(vs)
-    file = os.path.abspath(directory + vs[0])
+    for i, v in enumerate(vs):
+    # random.shuffle(vs)
+        file = os.path.abspath(directory + v)
 
-    #ビデオパスを入れる
-    save_file_name = 'cuttest.mp4' #保存ファイル名
-    start = 5
-
-    videos = VideoFileClip(file).subclip(start)
-    videos.write_videofile(save_file_name, fps=29, codec='libx264', audio_codec='aac', temp_audiofile='temp-audio.m4a', remove_temp=True)
+        #ビデオパスを入れる
+        save_file_name = f'/mnt/hdd/don/files/menes_fanza_video/cut{i}.mp4' #保存ファイル名
+        start = 5
+        try:
+            videos = VideoFileClip(file).subclip(start)
+            videos.write_videofile(save_file_name, fps=29, codec='libx264', audio_codec='aac', temp_audiofile='temp-audio.m4a', remove_temp=True)
+        except:
+            pass
 
 
 def cut2min():
