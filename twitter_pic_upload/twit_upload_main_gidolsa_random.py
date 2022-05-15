53097e10b1b823ed048bf2053f33c067819813db
tomato-ga <servicedake@gmail.com>
2022-05-15 23:16:38 +0900

    フォルダ追加

--- twitter_pic_upload/twit_upload_main_gidolsa_random.py
+++ twitter_pic_upload/twit_upload_main_gidolsa_random.py
@@ -10,7 +10,12 @@ if __name__ == '__main__':
     '#馬場ふみか': '/mnt/hdd/don/files/twitphotos/HoretaFumika/',
     '#横野すみれ': '/mnt/hdd/don/files/mizugazo/横野すみれ',
     '#浅川梨奈': '/mnt/hdd/don/files/mizugazo/浅川梨奈',
-    '#新垣結衣': '/mnt/hdd/don/files/mizugazo/新垣結衣'
+    '#新垣結衣': '/mnt/hdd/don/files/mizugazo/新垣結衣',
+    '#山本舞香': '/mnt/hdd/don/files/mizugazo/山本舞香',
+    '#宮脇咲良': '/mnt/hdd/don/files/mizugazo/宮脇咲良',
+    '#岸明日香': '/mnt/hdd/don/files/mizugazo/岸明日香',
+    '#貴島明日香': '/mnt/hdd/don/files/mizugazo/貴島明日香',
+    '#井川遥': '/mnt/hdd/don/files/mizugazo/井川遥'
     }
 
     hashtag, pic_dir = random.choice(list(photos.items()))
