817c488663904da9842d35c130d5eb88b8f05633
tomato-ga <servicedake@gmail.com>
2022-05-18 17:46:51 +0900

    Macでテストする

--- twitter_aff_videos/selenium_upload/twit_upload_main_togsi7.py
+++ twitter_aff_videos/selenium_upload/twit_upload_main_togsi7.py
@@ -1,4 +1,4 @@
-from master_twit_upload_module_0510 import Tweet
+from master_twit_upload_module_genre import Tweet
 import json
 import pandas as pd
 import random
@@ -17,8 +17,8 @@ if __name__ == '__main__':
     upload_url: str = upload_video_random[0]['aff_url']
     upload_file_name: str = upload_video_random[0]['file_name']
     upload_path_and_file_name: str = '/mnt/hdd/don/files/fanza/bust90to99_cut/' + upload_file_name
-    print(upload_file_name, ':', upload_url)
+    print(upload_path_and_file_name, ':', upload_url)
 
     i = Tweet()
-    i.Uploads(account='togsi7', text='いいね！')
+    i.Uploads(account='togsi7', text='いいね！', up_url=upload_url, up_file=upload_path_and_file_name)
     i.Quit()
