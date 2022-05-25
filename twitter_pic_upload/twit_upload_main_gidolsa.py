ddaea41a2e2fa6f02138fa6c29b34755f0474132
tomato-ga <servicedake@gmail.com>
2022-05-26 00:36:25 +0900

    修正

--- twitter_pic_upload/twit_upload_main_gidolsa.py
+++ twitter_pic_upload/twit_upload_main_gidolsa.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     pic_dir = '/mnt/hdd/don/files/twitphotos/yukihira/'
 
     i = Tweet()
-    i.Uploads(account='gidolsa', password='asdflkjh', text='#雪平莉左' + '\n' + '#かわいいと思ったらいいね', pic_dir=pic_dir)
+    i.Uploads(account='gidolsa', password='asdflkjh', text='#雪平莉左' + '\n' + '#かわいいと思ったらいいね' + '\n' + '#グラビア', pic_dir=pic_dir)
     i.Quit()
 
--- twitter_pic_upload/twit_upload_main_gidolsa_random.py
+++ twitter_pic_upload/twit_upload_main_gidolsa_random.py
@@ -11,14 +11,19 @@ if __name__ == '__main__':
     '#横野すみれ': '/mnt/hdd/don/files/mizugazo/横野すみれ/',
     '#浅川梨奈': '/mnt/hdd/don/files/mizugazo/浅川梨奈/',
     '#新垣結衣': '/mnt/hdd/don/files/mizugazo/新垣結衣/',
-    '#山本舞香': '/mnt/hdd/don/files/mizugazo/山本舞香/',
     '#宮脇咲良': '/mnt/hdd/don/files/mizugazo/宮脇咲良/',
     '#岸明日香': '/mnt/hdd/don/files/mizugazo/岸明日香/',
     '#貴島明日香': '/mnt/hdd/don/files/mizugazo/貴島明日香/',
-    '#井川遥': '/mnt/hdd/don/files/mizugazo/井川遥/'
+    '#井川遥': '/mnt/hdd/don/files/mizugazo/井川遥/',
+    '#桃月なしこ': '/mnt/hdd/don/files/mizugazo/桃月なしこ/',
+    '#福田ルミカ': '/mnt/hdd/don/files/mizugazo/福田ルミカ/',
+    '#本郷柚巴': '/mnt/hdd/don/files/mizugazo/本郷柚巴/',
+    '#小芝風花': '/mnt/hdd/don/files/mizugazo/小芝風花/',
+    # '#山本舞香': '/mnt/hdd/don/files/mizugazo/山本舞香/',
     }
 
     hashtag, pic_dir = random.choice(list(photos.items()))
+    assert os.path.isdir(pic_dir)
     print(hashtag, pic_dir)
 
 
