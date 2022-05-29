5c3080c001f4b27727316f501f3b1f8c3ac47bfa
tomato-ga <servicedake@gmail.com>
2022-05-29 21:35:41 +0900

    imgあるなしで分岐が変わる

---
+++ uraaka/yukarin_sef/twit_upload_main_yukarin.py
@@ -0,0 +1,11 @@
+from master_twit_photo_upload import Tweet
+
+
+if __name__ == '__main__':
+
+    pic_dir = '/home/don/py/DMM/uraaka/yukarin_sef/yukarinn_0214/'
+
+    i = Tweet()
+    i.Uploads(account='yukarin_sef', password='asdflkjh', pic_dir=pic_dir)
+    i.Quit()
+
