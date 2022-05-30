40dc21e1df5c0ed9601c742c6998a187245d441a
tomato-ga <servicedake@gmail.com>
2022-05-30 23:44:00 +0900

    SOKMILいけた

--- SOKMILAPI/sokmil_api.py
+++ SOKMILAPI/sokmil_api.py
@@ -43,44 +43,48 @@ class Sokmil:
         search_json_box = Box.from_json(search_keyword_response.text)
         print(search_json_box)
 
+        for i, x in enumerate(search_json_box.result['items']):
+            url = x['sampleMovieURL']['url']
+            yield i, url
 
-        url = search_json_box.result['items'][0]['sampleMovieURL']['url']
-        print(url)
-        return url
 
-
-class Down:
+class Down(Sokmil):
 
     def __init__(self):
         self.options = Options()
         self.options.add_argument('--headless')
+        self.options.add_argument('--ignore-certificate-errors')
+        self.options.add_argument('--lang=ja-JP')
+        self.options.add_argument('--no-sandbox')
         self.options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36')
         self.driver = webdriver.Chrome(options=self.options)
         self.wait = WebDriverWait(driver=self.driver, timeout=30)
 
-    def down(self, url):
+    def down(self, index, url):
 
         self.driver.get(url)
+        time.sleep(0.2)
         self.wait.until(EC.presence_of_all_elements_located)
-        time.sleep(0.5)
+        self.driver.save_screenshot('./1.png')
         source = self.driver.page_source
-        print(source)
 
-        url_search = re.findall("r'(https:)\\'", source) # (^https://)(\w.*)(.mp4$)
-        print(url_search)
+        search = re.findall(r'([https:\\\])(\w*\.\w*.\w*\\/\w*\\.?\?)(a=)([\w_-]*&h=\w*&\w=\w*.mp4)', source) # 毎回ファイル名がランダムだった
+
+        search_url_modify = search[0].replace('\\', '')
+        video_response = requests.get(search_url_modify)
+        with open(f'/mnt/hdd2/{index}.mp4', 'wb') as save_video:   # Ubuntu f'/mnt/hdd/don/files/twitvideo/ //  Win f'E:\\twitvideo\\{str(v_url_file_name)}.mp4',
+            save_video.write(video_response.content)
+        print(video_response)
 
 
 
 s = Sokmil()
+d = Down()
 s.APIKEY = 'f5fa0b38192389bb264ad32bdb80c802'
 s.AFFILIATEID = 24353
 s.hits_count = 20
 s.offset_count = 1
-s.keyword = '奈月セナ'
+s.keyword = '浅川梨奈'
 
-video_url = s.search_items()
-
-# video_url = 'https://www.sokmil.com/idol/_item/item297385.htm'
-
-d = Down()
-d.down(video_url)
+for i, video_url in s.search_items():
+    d.down(i, video_url)
