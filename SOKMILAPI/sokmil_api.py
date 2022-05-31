c8fb4225f730792201f5c9d23a2b32ec48a8baa5
tomato-ga <servicedake@gmail.com>
2022-05-31 14:17:51 +0900

    SOK JSON保存完了

--- SOKMILAPI/sokmil_api.py
+++ SOKMILAPI/sokmil_api.py
@@ -35,7 +35,6 @@ class Sokmil:
             'category': 'idol',
             'keyword': self.keyword,
             'output': 'json',
-
             }
 
         search_keyword_response = requests.get(api_url, params=url_params)
@@ -43,9 +42,18 @@ class Sokmil:
         search_json_box = Box.from_json(search_keyword_response.text)
         print(search_json_box)
 
+
+
         for i, x in enumerate(search_json_box.result['items']):
-            url = x['sampleMovieURL']['url']
-            yield i, url
+
+            yield dict(
+                title = x['title'],
+                actor = x.iteminfo.actor[0]['name'],
+                video_embed_url = x['sampleMovieURL']['url'],
+                aff_url =  x['affiliateURL'],
+                dates = x['date'],
+                price = x.prices['price'],
+                index=i)
 
 
 class Down(Sokmil):
@@ -60,23 +68,28 @@ class Down(Sokmil):
         self.driver = webdriver.Chrome(options=self.options)
         self.wait = WebDriverWait(driver=self.driver, timeout=30)
 
-    def down(self, index, url):
+    def down(self, video_info: dict):
 
-        self.driver.get(url)
+        video_embed_url = video_info['video_embed_url']
+        index = video_info['index']
+
+        self.driver.get(video_embed_url)
         time.sleep(0.2)
         self.wait.until(EC.presence_of_all_elements_located)
         self.driver.save_screenshot('./1.png')
         source = self.driver.page_source
 
         search = re.findall(r'([https:\\\])(\w*\.\w*.\w*\\/\w*\\.?\?)(a=)([\w_-]*&h=\w*&\w=\w*.mp4)', source) # 毎回ファイル名がランダムだった
+        file_name = f'{index}.mp4'
 
         search_url_modify = search[0].replace('\\', '')
         video_response = requests.get(search_url_modify)
-        with open(f'/mnt/hdd2/{index}.mp4', 'wb') as save_video:   # Ubuntu f'/mnt/hdd/don/files/twitvideo/ //  Win f'E:\\twitvideo\\{str(v_url_file_name)}.mp4',
+        os.makedirs(fr'/mnt/hdd/don/files/sok/{s.keyword}/', mode=0o777, exist_ok=True)
+        with open(fr'/mnt/hdd/don/files/sok/{s.keyword}/{file_name}', 'wb') as save_video:
             save_video.write(video_response.content)
         print(video_response)
 
-
+        return file_name
 
 s = Sokmil()
 d = Down()
@@ -84,7 +97,15 @@ s.APIKEY = 'f5fa0b38192389bb264ad32bdb80c802'
 s.AFFILIATEID = 24353
 s.hits_count = 20
 s.offset_count = 1
-s.keyword = '浅川梨奈'
+s.keyword = 'MEGUMI'
 
-for i, video_url in s.search_items():
-    d.down(i, video_url)
+save_json = {}
+save_json['title'] = []
+
+for video_info in s.search_items():
+    file_name = d.down(video_info)
+    video_info['file_name'] = file_name
+    save_json['title'].append(video_info)
+
+with open(fr'/mnt/hdd/don/files/sok/{s.keyword}/sokmil_{s.keyword}_videofile.json', 'w+', encoding='utf-8') as f:
+    json.dump(save_json, f, indent=4, ensure_ascii=False)
